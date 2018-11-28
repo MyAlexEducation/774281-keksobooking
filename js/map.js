9a0309a98a04b545d02ec631439258815eb55ce0
Alex <MyAlexEducation@gmail.com>
2018-11-28 12:48:01 +0300

    определяет данные и константы

---
+++ js/map.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var PATH_AVATAR_IMGS = 'img/avatars/';
+var MIN_PRICE = 1000;
+var MAX_PRICE = 1000000;
+var MIN_ROOMS = 1;
+var MAX_ROOMS = 5;
+var MIN_LOCATION_Y = 130;
+var MAX_LOCATION_Y = 630;
+var avatar_imgs = ['01.png', '02.png', '03.png', '04.png', '05.png', '06.png', '07.png', '08.png'];
+var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var types = ['palace', 'flat', 'house', 'bungalo'];
+var checkin = ['12:00', '13:00', '14:00'];
+var checkout = ['12:00', '13:00', '14:00'];
+var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
