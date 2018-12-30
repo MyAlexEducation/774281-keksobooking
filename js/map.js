25e7db65ec2b9e45fb1b7b28d4a7b42ab3f598e0
Alex <MyAlexEducation@gmail.com>
2018-12-30 11:52:17 +0300

    реализует перетаскивание главной метки

--- js/map.js
+++ js/map.js
@@ -180,16 +180,62 @@ var adFormTimeInit = function () {
   adFormTimeIn.options[0].selected = true;
   adFormTimeOut.options[0].selected = true;
 };
+var adFormAddressInit = function () {
+  adFormAddress.value = WIDTH_MAP / 2 + ',' + HEIGHT_MAP / 2;
+};
 
 fillArray(buildings, Building, NUMBER_ADS);
 createPins(buildings);
 
-adFormAddress.value = WIDTH_MAP / 2 + ',' + HEIGHT_MAP / 2;
-mainPin.addEventListener('mouseup', function () {
+mainPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+    mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        dialogHandler.removeEventListener('click', onClickPreventDefault)
+      };
+      dialogHandler.addEventListener('click', onClickPreventDefault);
+    }
+
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
   showMap();
   showAdForm();
   showPins();
 });
+
 map.addEventListener('click', function (evt) {
   var target = evt.target;
   while (target !== map) {
@@ -208,6 +254,7 @@ map.addEventListener('click', function (evt) {
   }
 });
 
+adFormAddressInit();
 adFormCapacityInit();
 adFormPriceInit();
 adFormTypeInit();
